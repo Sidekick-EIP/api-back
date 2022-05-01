4cf720012227385cdef60ad33f665aac66c0060b
Theo <poette.theo@gmail.com>
2022-05-01 14:52:39 +0200

    feat: setup of auth module with basic routes and startup of application

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { getEnvPath } from './common/helper/env.helper';
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 const envFilePath = getEnvPath(`${__dirname}`);
 
 @Module({
-  imports: [ConfigModule.forRoot({envFilePath, isGlobal: true}), PrismaModule],
+  imports: [ConfigModule.forRoot({envFilePath, isGlobal: true}), PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
