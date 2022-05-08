b38e2ad1ce9a07e1e49590d82742faf18073aad7
guts <pierre.bittou@epitech.eu>
2022-05-08 16:43:02 +0200

    [~+] : creation of user_infos.module.ts, modification of the service and controller

--- src/user_infos/user_infos.service.ts
+++ src/user_infos/user_infos.service.ts
@@ -1,1 +1,14 @@
-test;
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserInfoService {
+    constructor() {}
+
+    public getUserInfo() {
+        console.log("getter");
+    }
+
+    public setUserInfo() {
+        console.log("setter");
+    }
+}
