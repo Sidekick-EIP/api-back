6c1517211fbf386b45f94e9f13d6d9423aa3bd68
Theo <poette.theo@gmail.com>
2022-05-22 16:10:56 +0200

    feat: wip auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,30 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Public } from 'src/common/decorators';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post("login")
-  login() {
-    this.authService.login();
+  login(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
   }
 
   @Post("register")
-  register() {
-    this.authService.register();
+  register(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
+  }
+
+  @Get("me")
+  me() {
+    return "connected";
+  }
+
+  @Public()
+  @Get("public")
+  public() {
+    return "public";
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -6,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(configService.get("app.port") || 8080);
 }
 bootstrap();
