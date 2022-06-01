047656240e9325f33d5c26cda5fff72734c39688
poulet frites <alexandre.antoniutti@epitech.eu>
2022-06-01 23:54:02 +0200

    (feat/main) : Add migration table userData

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,16 +6,8 @@ const prisma = new PrismaClient()
 async function main() {
   const user = await prisma.user.create({
     data: {
-      username: "test",
       email: 'test@gmail.com',
       password: await argon.hash('password'),
-      firstname: "Test",
-      lastname: "LeTest",
-      size: 180,
-      weight: 75,
-      gender: 'MALE',
-      description: 'Testttttttt',
-      sport_frequence: 'TWICE_A_WEEK'
     }
   })
 }
