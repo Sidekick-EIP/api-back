4c99e4bef8f08e0926c5e6eb8f169a201008f4cc
Theo <poette.theo@gmail.com>
2022-06-03 21:31:31 +0200

    feat: correct env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { getEnvPath } from './common/helper/env.helper';
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,10 +8,8 @@ import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './common/guards';
 import { FormModule } from './form/form.module';
 
-const envFilePath = getEnvPath(`${__dirname}`);
-
 @Module({
-  imports: [ConfigModule.forRoot({envFilePath, isGlobal: true}), PrismaModule, AuthModule, FormModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), PrismaModule, AuthModule, FormModule],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_GUARD,
