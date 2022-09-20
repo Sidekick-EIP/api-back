57551e7db72438d199945f7b8a273e161d043482
poulet frites <alexandre.antoniutti@epitech.eu>
2022-09-20 16:37:50 +0200

    Ajouts de l'email des la route des users infos

--- src/form/form.controller.ts
+++ src/form/form.controller.ts
@@ -1,17 +1,13 @@
 import { FormDto } from './dto/form.dto';
 import { FormService } from './form.service';
 import { Body, Controller, Post } from '@nestjs/common';
-import { Public } from '../common/decorators';
-
 
 @Controller('form')
 export class FormController {
   constructor(private formservice: FormService) { }
 
-  @Public()
   @Post('save')
   async saveFormDatas(@Body() dto: FormDto, userId: string) {
     return await this.formservice.saveFormDatas(dto, userId);
   }
-  
 }
