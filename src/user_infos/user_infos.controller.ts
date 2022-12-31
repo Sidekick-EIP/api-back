30b9c00f329e88512d3a35a5a641e7cc737b7cb2
Theo <poette.theo@gmail.com>
2022-12-30 19:09:16 -0500

    feat: update infos

--- src/user_infos/user_infos.controller.ts
+++ src/user_infos/user_infos.controller.ts
@@ -1,25 +1,30 @@
-import { Body, Controller, Get, Post, Query, Request} from '@nestjs/common';
-import { UserInfoService } from './user_infos.service';
-import {UserInfosDto } from './dto/user.dto';
-import { Public } from '../common/decorators';
+import { Body, Controller, Get, Post, Query, Request } from "@nestjs/common";
+import { UserInfoService } from "./user_infos.service";
+import { UserInfosDto } from "./dto/user.dto";
+import { Public } from "../common/decorators";
 
-@Controller('user_infos')
+@Controller("user_infos")
 export class UserInfosController {
-    constructor(private userInfoService: UserInfoService) { }
+  constructor(private userInfoService: UserInfoService) {}
 
-    @Get("getUserInfos")
-    getUserInfos(@Request() req: any) {
-      return this.userInfoService.getUserInfoById(req.user.email);
-    }
+  @Get("getUserInfos")
+  getUserInfos(@Request() req: any) {
+    return this.userInfoService.getUserInfoById(req.user.email);
+  }
 
-    @Post("setUserInfos")
-    setUserInfos(@Request() req: any, @Body() dto: UserInfosDto) {
-      return this.userInfoService.setUserInfo(dto, req.user.email);
-    }
+  @Post("setUserInfos")
+  setUserInfos(@Request() req: any, @Body() dto: UserInfosDto) {
+    return this.userInfoService.setUserInfo(dto, req.user.email);
+  }
 
-    @Public()
-    @Post("link_users")
-    linkUsers(@Body() req: {id1: string, id2: string}) {
-      return this.userInfoService.linkUsers(req);
-    }
+  @Post("update/infos")
+  updateInfos(@Request() req: any, @Body() dto: UserInfosDto) {
+    return this.userInfoService.updateInfos(dto, req.user.email);
+  }
+
+  @Public()
+  @Post("link_users")
+  linkUsers(@Body() req: { id1: string; id2: string }) {
+    return this.userInfoService.linkUsers(req);
+  }
 }
