e2ad633f1bdeeff83f6d392331f6e9272ccfb62e
Theo <poette.theo@gmail.com>
2022-12-31 11:11:44 -0500

    feat: update infos

--- src/user_infos/dto/edit.dto.ts
+++ src/user_infos/dto/edit.dto.ts
@@ -1,30 +1,23 @@
 import { Gender, SportFrequence } from "@prisma/client";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsString } from "class-validator";
 
-export class UserInfosDto {
-  @IsNotEmpty()
+export class EditInfosDto {
   @IsString()
   firstname: string;
 
-  @IsNotEmpty()
   @IsString()
   lastname: string;
 
-  @IsNotEmpty()
   @IsString()
   description: string;
 
-  @IsNotEmpty()
   @IsString()
   size: number;
 
-  @IsNotEmpty()
   @IsString()
   weight: number;
 
-  @IsNotEmpty()
   gender: Gender;
 
-  @IsNotEmpty()
   sport_frequence: SportFrequence;
 }
--- src/user_infos/user_infos.controller.ts
+++ src/user_infos/user_infos.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post, Query, Request } from "@nestjs/common";
 import { UserInfoService } from "./user_infos.service";
 import { UserInfosDto } from "./dto/user.dto";
 import { Public } from "../common/decorators";
+import { EditInfosDto } from "./dto/edit.dto";
 
 @Controller("user_infos")
 export class UserInfosController {
@@ -18,7 +19,7 @@ export class UserInfosController {
   }
 
   @Post("update/infos")
-  updateInfos(@Request() req: any, @Body() dto: UserInfosDto) {
+  updateInfos(@Request() req: any, @Body() dto: EditInfosDto) {
     return this.userInfoService.updateInfos(dto, req.user.email);
   }
 
