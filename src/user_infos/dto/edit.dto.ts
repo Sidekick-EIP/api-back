f03ba2c12d6d414753908dfbe3fdf87cef943401
Theo <poette.theo@gmail.com>
2022-12-31 12:00:32 -0500

    feat: update infos

--- src/user_infos/dto/edit.dto.ts
+++ src/user_infos/dto/edit.dto.ts
@@ -2,22 +2,29 @@ import { Gender, SportFrequence } from "@prisma/client";
 import { IsOptional, IsString } from "class-validator";
 
 export class EditInfosDto {
+  @IsOptional()
   @IsString()
   firstname?: string;
 
+  @IsOptional()
   @IsString()
   lastname?: string;
 
+  @IsOptional()
   @IsString()
   description?: string;
 
+  @IsOptional()
   @IsString()
   size?: number;
 
+  @IsOptional()
   @IsString()
   weight?: number;
 
+  @IsOptional()
   gender?: Gender;
 
+  @IsOptional()
   sport_frequence?: SportFrequence;
 }
