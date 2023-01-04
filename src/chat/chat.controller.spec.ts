60e9b6d432827c67d9d2e1a26abc04b94fa37b80
Theo <poette.theo@gmail.com>
2023-01-03 19:33:02 -0500

    fix: tests

--- src/chat/chat.controller.spec.ts
+++ src/chat/chat.controller.spec.ts
@@ -1,5 +1,9 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserInfoService } from '../user_infos/user_infos.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { ChatController } from './chat.controller';
+import { ChatService } from './chat.service';
 
 describe('ChatController', () => {
   let controller: ChatController;
@@ -7,6 +11,7 @@ describe('ChatController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChatController],
+      providers: [PrismaService, ConfigService, ChatService, UserInfoService],
     }).compile();
 
     controller = module.get<ChatController>(ChatController);
