8609199895eb070613101a31e2995fadde79eb96
Theo <poette.theo@gmail.com>
2023-01-25 16:32:42 -0500

    fix: migration

--- prisma/migrations/20230125105256_/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "meals" ALTER COLUMN "period" SET DEFAULT 'dinner';
-
--- AlterTable
-ALTER TABLE "users_data" ALTER COLUMN "birthDate" SET DEFAULT CURRENT_TIMESTAMP,
-ALTER COLUMN "username" SET DEFAULT '';
--- prisma/migrations/20230125213139_meals/migration.sql
+++ prisma/migrations/20230125213139_meals/migration.sql
@@ -5,20 +5,17 @@
   - You are about to drop the column `lipids` on the `meals` table. All the data in the column will be lost.
   - You are about to drop the column `proteins` on the `meals` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[id]` on the table `meals` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `period` to the `meals` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `birthDate` to the `users_data` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `username` to the `users_data` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "meals" DROP COLUMN "carbohydrates",
 DROP COLUMN "lipids",
 DROP COLUMN "proteins",
-ADD COLUMN     "period" TEXT NOT NULL;
+ADD COLUMN     "period" TEXT NOT NULL DEFAULT 'dinner';
 
 -- AlterTable
-ALTER TABLE "users_data" ADD COLUMN     "birthDate" TIMESTAMP(3) NOT NULL,
-ADD COLUMN     "username" TEXT NOT NULL;
+ALTER TABLE "users_data" ADD COLUMN     "birthDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "username" TEXT NOT NULL DEFAULT '';
 
 -- CreateIndex
 CREATE UNIQUE INDEX "meals_id_key" ON "meals"("id");
