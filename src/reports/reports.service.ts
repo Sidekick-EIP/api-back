9f84f22511cd2bb5b8b695b238ab7ef49c165120
Theo <poette.theo@gmail.com>
2023-02-09 14:50:38 -0500

    feat: delete sidekicks

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,29 +1,52 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class ReportsService {
   constructor(private prisma: PrismaService) {}
 
   async create(reporter: string, reason: string) {
-    const user = await this.prisma.userData.findUnique({
+    const user = await this.prisma.user.findUnique({
+      where: { email: reporter },
+    });
+
+    const userInfo = await this.prisma.userData.findUnique({
       where: {
-        userId: reporter,
+        userId: user.id,
       },
     });
 
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException("User not found");
     }
 
-    if (!user.sidekick_id)
-      throw new NotFoundException('User does not have a sidekick');
+    if (!userInfo.sidekick_id)
+      throw new NotFoundException("User does not have a sidekick");
+
+    await Promise.all([
+      this.prisma.userData.update({
+        where: {
+          userId: userInfo.sidekick_id,
+        },
+        data: {
+          sidekick_id: null,
+        },
+      }),
+      this.prisma.userData.update({
+        where: {
+          userId: user.id,
+        },
+        data: {
+          sidekick_id: null,
+        },
+      }),
+    ]);
 
     return await this.prisma.reports.create({
       data: {
-        reason: reason,
-        reporterId: reporter,
-        userId: user.sidekick_id,
+        reason: reason ?? "No reason provided",
+        reporterEmail: reporter,
+        userId: userInfo.sidekick_id,
       },
     });
   }
