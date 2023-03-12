3b28ea4590400b28f30cb1885c700b6e3cce5740
pouletfrites <pouletfrites@DESKTOP-4GO5QJA>
2023-03-12 16:48:33 +0100

    add beginning of tests users

--- src/common/decorators/current_user.decorator.ts
+++ src/common/decorators/current_user.decorator.ts
@@ -1,4 +1,6 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PrismaService } from "src/prisma/prisma.service";
 
 export const GetCurrentUserEmail = createParamDecorator(
   (data: undefined, context: ExecutionContext) => {
@@ -6,3 +8,17 @@ export const GetCurrentUserEmail = createParamDecorator(
     return request?.user?.email;
   }
 )
+
+// export const GetCurrentUserId = createParamDecorator(
+//   async (data: undefined) => {
+//     const config = new ConfigService()
+//     const prisma = new PrismaService(config)
+
+//     const res = await prisma.user.findUnique({
+//       where: {
+//         email: data,
+//       },
+//     });
+//     return res.id;
+//   }
+// )
---
+++ src/user_infos/user_infos.service.spec.ts
@@ -0,0 +1,49 @@
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { PrismaService } from "../prisma/prisma.service";
+import { Test, TestingModule } from "@nestjs/testing";
+import { UserInfoService } from "./user_infos.service";
+
+describe("AuthService", () => {
+	let service: UserInfoService;
+	let prisma: PrismaService;
+  
+	beforeEach(async () => {
+	  const module: TestingModule = await Test.createTestingModule({
+		providers: [
+		  UserInfoService,
+		  PrismaService,
+		  ConfigService,
+		  JwtService,
+		],
+	  }).compile();
+  
+	  prisma = module.get<PrismaService>(PrismaService);
+	  service = module.get<UserInfoService>(UserInfoService);
+	});
+  
+	it("should be defined", async () => {
+	  expect(service).toBeDefined();
+	  expect(prisma).toBeDefined();
+	});
+
+	it("should create user infos", async () => {
+	  
+	})
+
+	it("should modify user infos", async () => {
+	  
+	})
+
+	it("should delete user infos", async () => {
+	  
+	})
+
+	it("should get user infos from Email", async () => {
+	  
+	})
+
+	it("should link users", async () => {
+	  
+	})
+  });
