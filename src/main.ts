4fbe7d7773cee6f002272f09224b467ca7be344b
Theo <poette.theo@gmail.com>
2023-05-26 16:10:30 +0200

    feat: payload size

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as multer from "multer";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  const limits = {
+    fileSize: 1024 * 1024 * 10,
+  };
+  const multerMiddleware = multer({ limits });
+  app.use(multerMiddleware.any());
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
