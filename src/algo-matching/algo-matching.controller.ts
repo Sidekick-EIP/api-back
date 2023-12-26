284cb577fae8490aefc5e8738a1bf0698e45cc82
Theo <poette.theo@gmail.com>
2023-12-26 17:30:49 +0100

    fix: remove useless code

--- src/algo-matching/algo-matching.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { AlgoMatchingService } from './algo-matching.service';
-
-@Controller('algo-matching')
-export class AlgoMatchingController {
-  constructor(private readonly algoMatchingService: AlgoMatchingService) {
-
-  }
-}
